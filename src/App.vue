95895dae2660effc1dffecc546131b0fe0ce354a
Kyle <leungkyle2@gmail.com>
2023-07-04 20:12:59 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+    </div>
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1 class="text-3xl font-bold underline bg-red-500">hello world</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-evenly">
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Skills</a>
+    <a href="">Work</a>
+    <a href="">Contact</a>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<!-- sample vue component
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="">
+  </div>
+</template>
+
+<style scoped>
+</style> -->
